e3227e6bfd28471d3b279485557b8ce45d20eaa9
truediegomarques <56486783+truediegomarques@users.noreply.github.com>
2024-02-17 01:46:43 -0300

    Subindo para teste no windows

--- app001.py
+++ app001.py
@@ -1,2 +1,24 @@
-#teste
-#teste novo
+# pandas
+#from sys import displayhook
+import pandas as pd
+
+tabela = pd.read_excel("/home/diego/wks_vscode/estudo_python/PythonExcel/Produtos.xlsx")
+
+df = pd.DataFrame(tabela)
+
+tam = len(df.index)
+ind = 0
+valor_acumulado = 0 
+
+while ind < tam:
+    
+    linha = df.loc[ind]
+    descricao = linha['produtos']
+    preco = linha['preco']
+    tipo = linha['tipo']
+    imposto = linha['imposto']
+    final = preco * imposto
+    ind = ind + 1
+    print(f"Produto {descricao} preco final {final:,.2f}")
+    
+    
